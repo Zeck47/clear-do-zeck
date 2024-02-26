0f9b16b7394be6b212b2eb50c1c42cd18382aa3d
ZeckDev <122693027+Zeck47@users.noreply.github.com>
2024-02-26 16:48:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const {Client} = require("discord.js-selfbot-v11");
 const client = new Client();
 const request = require("request");
-const Webhook = "https://discordapp.com/api/webhooks/1210055879123013662/n-pKeE597ADz3RCuC0RvJKAIA6SUcQQXqnfNMVEekrR2zpbBDvdt3Jm2x4mXFh1VRZd8"
 const colors = require("colors");
 
 const {token,gatilho} = require("./zeck.json");
